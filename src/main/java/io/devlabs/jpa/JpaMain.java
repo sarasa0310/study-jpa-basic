310a2634eb884152748282a96d3993ce53bec690
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-07-23 16:29:34 +0900

    20230723 Section02.JPA 시작하기 완료

---
+++ src/main/java/io/devlabs/jpa/JpaMain.java
@@ -0,0 +1,36 @@
+package io.devlabs.jpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+            for (Member member : members) {
+                System.out.println(member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+}
